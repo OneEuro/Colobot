c4d1ea39c140cadeba81eb3451ca0ff3f35e66e9
Malyshev Vova <malyshewova@gmail.com>
2020-07-04 15:14:39 +0300

    add script PlayerMovement

---
+++ Assets/Scripts/Game/Units/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private CharacterController characterController;
+	private Animator animator;
+
+	[SerializeField]
+	private float moveSpeed = 100;
+	[SerializeField]
+	private float turnSpeed = 5f;
+
+	private void Awake()
+	{
+		characterController = GetComponent<CharacterController>();
+		animator = GetComponentInChildren<Animator>();
+	}
+
+	private void Update()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+
+		var movement = new Vector3(horizontal, 0, vertical);
+
+		characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+
+		animator.SetFloat("Speed", movement.magnitude);
+
+		if (movement.magnitude > 0)
+		{
+			Quaternion newDirection = Quaternion.LookRotation(movement);
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+		}
+	}
+}
